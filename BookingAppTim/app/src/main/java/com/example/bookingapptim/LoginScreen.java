929597ae7a75c15993bc4c09b5a4ab91566e0f21
matijap59 <matijap59@gmail.com>
2023-10-29 19:28:02 +0100

    add gui for login and register

---
+++ BookingAppTim/app/src/main/java/com/example/bookingapptim/LoginScreen.java
@@ -0,0 +1,89 @@
+package com.example.bookingapptim;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+        Button btnLogin= findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginScreen.this,HomeScreen.class);
+                startActivity(intent);
+
+            }
+        });
+
+        Button btnRegister= findViewById(R.id.tvRegister);
+        btnRegister.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginScreen.this,RegisterScreen.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("BookingAppTim", "LoginActivity onStart()");
+    }
+    /*
+     * onResume se poziva kada je aktivnost u fokusu i korisnik
+     * je u interakciji sa aktivnosti.
+     * */
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d("BookingAppTim", "LoginActivity onResume()");
+    }
+
+    /*
+     * onPause se poziva kada je aktivnost delimicno prekrivena.
+     * */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("BookingAppTim", "LoginActivity onPause()");
+    }
+    /*
+     * onStop se poziva kada je aktivnost u potpunosti prekrivena nekom drugom aktivnošću
+     * */
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("BookingAppTim", "LoginActivity onStop()");
+    }
+    /*
+     * onDestory se poziva kada je aktivnost u potpunosti unistena,
+     * ondosno kada je aplikacija zatvorena
+     * Izbrisana je iz background-a.
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("BookingAppTim", "LoginActivity onDestroy()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("BookingAppTim", "LoginActivity onRestart()");
+    }
+
+
+
+}
